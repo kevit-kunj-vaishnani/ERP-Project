b0ad3025d1edb846133201f9e4dc7aecb4ae2ab1
kevit-kunj-vaishnani <kevit.kunj.vaishnani@gmail.com>
2023-10-20 11:15:04 +0530

    mongoose connection setup add

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ require('dotenv').config();
 import express, {Application, Router} from 'express';
 import {server} from './config';
 import {logger} from './utils/logger';
+import mongoose from 'mongoose';
+
+const mongoUrl: string = server.url;
 
 export class App {
   private app: Application;
@@ -11,17 +14,38 @@ export class App {
     this.routers = routers;
     this.app = express();
     this.initializeRoutes();
+    this.mongoSetup();
   }
 
+  // initializeRoutes method declared here which is called from constructor
   initializeRoutes() {
     this.routers.forEach((router) => {
       this.app.use('/', router);
     });
   }
 
+  // listen method declared here which is called from constructor for listening port
   listen() {
     this.app.listen(server.port, () => {
       logger.info(`app is running on port ${server.port}`);
     });
+  }
+
+  // mongoSetup method declared here which is called from constructor for connecting with database
+  public mongoSetup() {
+    mongoose.connection.on('connected', () => {
+      logger.info('Database Connected Successfully');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      logger.info(`Database error : ${err}`);
+    });
+
+    const dbOptions = {
+      maxPoolSize: 5,
+      useNewUrlParser: true
+    };
+
+    mongoose.connect(mongoUrl, dbOptions);
   }
 }
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
-const {PORT, LOG_LEVEL} = process.env;
+const {PORT, LOG_LEVEL, MONGODB_URL} = process.env; // destructuring
 
 export const server = {
   port: PORT || 3000,
-  logLevel: LOG_LEVEL || 'info'
+  logLevel: LOG_LEVEL || 'info',
+  url: MONGODB_URL || 'mongodb://127.0.0.1:27017/mongodb://127.0.0.1:27017/ERP_College_Model'
 };
